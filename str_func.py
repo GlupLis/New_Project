12b28d87a510c33aa94777907259beacce6b4729
GlupLis <yushkov-vv@mail.ru>
2024-05-14 05:07:03 +0700

    a second function was added and docstrings were written

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def uppercase_text(t) -> str:
+    """
+    Фунция возвращает все слова в верхнем регистре
+    """
+
     return t.upper()
+
+
+def first_capital(s) -> str:
+    """
+    Функция возвращает каждое новое слово с заглавной буквы
+    """
+
+    return s.title()
